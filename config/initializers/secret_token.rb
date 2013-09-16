79c40658f90edf43420dd1e7f1c5180766005d09
alex@laptop <karasev.alexey@gmail.com>
2013-09-16 23:12:03 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hamster::Application.config.secret_key_base = '0bf7afa2a266b30e39fdd04f37078648b3a2f418d378aec1b72ed09838a3c5b41d24de247f843c1fa1eaa1842de1876e891e5002e7cff66f1a870f527cbddf58'
