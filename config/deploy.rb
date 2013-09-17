76df1b183ac32fcacd855fff2bb8fd874009ecbf
alex@laptop <karasev.alexey@gmail.com>
2013-09-18 01:16:57 +0400

    updated capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,13 +19,13 @@ role :db,  domain, :primary => true
 # deploy config
 set :deploy_to, applicationdir
 set :deploy_via, :export
-set :use_sudo, false
+#set :use_sudo, false
 set :ssh_options, { :forward_agent => true }
  
 # additional settings
 default_run_options[:pty] = true  # Forgo errors when deploying from windows
 #ssh_options[:keys] = %w(/home/user/.ssh/id_rsa)
-
+after "deploy", "deploy:migrate"
 
 
 
@@ -47,19 +47,21 @@ after "deploy:restart", "deploy:cleanup"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ namespace :deploy do
+   task :start do run "sudo /etc/init.d/thin start" end
+   task :stop do run "sudo /etc/init.d/thin stop" end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} /etc/init.d/thin restart"
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
 
-after 'deploy:update_code', 'deploy:symlink_db'
+before 'deploy:assets:precompile', 'deploy:symlink_db'
 
 namespace :deploy do
   desc "Symlinks the database.yml"
   task :symlink_db, :roles => :app do
     run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{deploy_to}/shared/sqlite3/production.sqlite3 #{release_path}/db/production.sqlite3"
   end
 end
