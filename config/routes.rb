4579a02b4133abfe2883e8e0d1968712bdfc6277
alex@laptop <karasev.alexey@gmail.com>
2013-09-23 00:42:53 +0400

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Hamster::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'leads#index'
+  root 'leads#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
