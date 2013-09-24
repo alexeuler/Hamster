8002deb0118557577620c1b4198a90e0a89e3bda
alex@laptop <karasev.alexey@gmail.com>
2013-09-25 00:33:25 +0400

    added thank you letter

--- app/mailers/info_mailer.rb
+++ app/mailers/info_mailer.rb
@@ -6,4 +6,10 @@ def new_lead(lead)
   @lead=lead
   mail(to:'info@homa-online.ru', subject:'Новый заказ')
 end
+
+def thank_you(to,name,sex)
+  @name=name
+  @sex=sex
+  mail(to:'info@homa-online.ru', subject:'Спасибо за заказ!')
+end
 end
